1d7943e534c41096a5f950a236846244d4470bac
Ayaka Neko <neko@ayaka.moe>
2020-10-20 20:19:36 +0800

    :art: Updated pool and added claim btn

---
+++ src/hooks/useClaim.ts
@@ -0,0 +1,28 @@
+import { useCallback } from 'react'
+import web3 from '../web3'
+import RefReward from '../constants/abi/RefReward.json'
+
+import { useWallet } from 'use-wallet'
+
+const useClaim = () => {
+  const { account } = useWallet()
+  const refRewardAddress = '0x62d24e413B24098BB8d2C19b64BCEf7aa981C755'
+
+  const contract = new web3.eth.Contract(RefReward as any, refRewardAddress)
+
+  const handleClaim = useCallback(
+    async () => {
+      const call = contract.methods.claim().send({ from: account })
+      const txHash = call.on('transactionHash', (tx: any) => {
+        console.log(tx)
+        return tx.transactionHash
+      })
+      console.log(txHash)
+    },
+    [account, contract],
+  )
+
+  return { onClaim: handleClaim, refRewardContract: contract }
+}
+
+export default useClaim
---
+++ src/views/Referral/components/Claim.tsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useState } from 'react'
+import styled from 'styled-components'
+import Button from '../../../components/Button'
+import useClaim from '../../../hooks/useClaim'
+
+const Claim: React.FC = () => {
+    const { onClaim } = useClaim()
+
+    return (
+        <StyledClaimButton>
+           <Button onClick={onClaim} size="sm" text="Claim" />
+       </StyledClaimButton>
+    )
+}
+
+const StyledClaimButton = styled.div`
+    padding-top: 10px;
+`
+
+export default Claim
